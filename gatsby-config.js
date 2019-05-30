06f01bfe636cbba0c0bf5b9fdda21d4627915bf6
Nico Vignola <nicolantonio.vignola@getyourguide.com>
2019-05-30 20:06:17 +0200

    Fix: add GA in head tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       options: {
         trackingId: 'UA-141149776-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        // head: false,
+        head: true,
         // Setting this parameter is optional
         // anonymize: true,
         // Setting this parameter is also optional
