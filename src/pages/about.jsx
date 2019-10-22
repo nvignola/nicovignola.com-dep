bdac5c01d68e91bccd0fc8280023164d51b66602
Nico Vignola <nicolantonio.vignola@getyourguide.com>
2019-10-22 14:16:13 +0200

    Bhavna: fix grammar

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,27 +11,26 @@ const About = center => (
     <Container>
       <h3>About me what?</h3>
       <p>
-        A very long time ago, a future rockstar tech guy born... was not me but{' '}
+        A very long time ago, a future rockstar tech guy was born... was not me but{' '}
         <a
           href="https://twitter.com/getify"
           title="go to Kyle Simposon twitter profile"
         >
           Kyle Simpson
         </a>
-        ... and after some other time, I manage to arrive in this world and my
-        story start from that point in history.
+        ... and after some other time, I managed to arrive in this world and my
+        story started from that point in history.
       </p>
       <p>
-        I've born in a city called Bari in the south of Italy in a region called
-        Puglia. When I was 8 years old, I and my parents decided to move to
-        Rimini, because in the small city where we were living, Gravina in
-        Puglia, not because there was no work for them but because of the
-        internet connection that was not so fast.
+        I was born in a city called Bari in the south of Italy in a region called
+        Puglia. When I was 8 years old, my parents and I decided to move to
+        Rimini from Gravina in Puglia.  We moved not because there was no work for them, but due to the slow internet connection there. 
+        
       </p>
       <p>
-        If you manage to read until this line, well done, you are very
-        interested in my life, thank you for that but I'll stop talking about it
-        I'm sorry but my biography will be out soon,{' '}
+        If you have managed to read until this line, well done, you are very
+        interested in my life. Thank you for that, but I will stop talking about it.
+        I am sorry but my biography will be out soon,{' '} 
         <a
           href="https://www.linkedin.com/in/nicolantonio-vignola/"
           target="_blank"
@@ -41,8 +40,8 @@ const About = center => (
         </a>
       </p>
       <p>
-        Let's stay focus on what matters. My real name is Nicolantonio but if
-        you can't deal with it Nico is more than fine.
+        Let us stay focussed on what matters. My real name is Nicolantonio but if
+        you can't deal with it, Nico is more than fine.
       </p>
       <h3>About this blog what?</h3>
       <p>
@@ -54,7 +53,7 @@ const About = center => (
         >
           the ghost who code
         </a>
-        ? Well, that answer the why of this blog mainly. I strongly suggest
+        ? Well, that answers the why of this blog. I strongly suggest
         reading that article... strongly.
       </p>
       <p>
@@ -64,10 +63,10 @@ const About = center => (
         but I'll try to share what I know and especially what I don't know.
       </p>
       <p>
-        This blog is also for me, my selfish healthy part. Since I can't retain
-        much information in my head I'll put it here.
+        This blog is also for me, my selfish, healthy part. Since I can't retain
+        much information in my head I will put it here.
       </p>
-      <p>So that's it, you can go back to whatever brought you here.</p>
+      <p>So that is it, you can go back to whatever brought you here.</p>
     </Container>
   </Layout>
 );
